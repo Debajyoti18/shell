02f6ac5126ed2e7386c12ffe8e48baf37531f3d6
Debajyoti18 <bdebajyoti22@gmail.com>
2025-06-21 16:35:09 +0530

    adding input output commands

--- script2.sh
+++ script2.sh
@@ -35,3 +35,23 @@ echo "Name: $name"
 echo "Age: $age"
 
 
+#   Various options opf the read command 
+#   -d <delimiter> 
+echo "hello , read until '#' :"
+read -d '#' inp
+echo "you entered before # is : $inp"
+
+#   -n <number> Reads up to the specified number of characters OR until the delimiter.
+echo "Enter up to 5 characters (input may end early with Enter):"
+read -n 5 input
+echo -e "\nYou entered: $input"
+
+#   -N <number> Reads exactly the number of characters, no delimiter (like Enter) is considered.
+read -N 5 inp1 #exactly read the 5 character not the enter 
+#   -p <prompt> Prints a prompt before taking input.
+#   -s Silent mode (no echo), useful for passwords.
+#   -a reads from indexed array 
+#   -t <time> : exaxtly wait for the given time for the input 
+#   -u <lines> :reads from file descriptor
+
+
